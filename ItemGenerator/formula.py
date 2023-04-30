d516ef4d097a076e3aaa75c0bd90048e6d20173d
ravielze <ravielz20@gmail.com>
2023-05-01 03:46:36 +0700

    Regenerate

--- ItemGenerator/formula.py
+++ ItemGenerator/formula.py
@@ -2,6 +2,11 @@ def pi(level, base, b, c):
     result = 0.0141 * level * level * (1.1 ** (0.1 * (c-1))) + 0.4969 * level * (1.2 ** (0.1 * (b-1))) + 8.49 + (base-9)
     return round(result, 2)
 
+def pio(level, offset, base, b, c):
+    lx = level+offset
+    result = 0.0141 * lx * lx * (1.1 ** (0.1 * (c-1))) + 0.4969 * lx * (1.2 ** (0.1 * (b-1))) + 8.49 + (base-9)
+    return round(result, 2)
+
 def color(level):
     switcher = {
         1: '<#23f108>',
